efa49bb44845a336bf18e208242bef94a15ba798
Austin Moore <am058613@ohio.edu>
2019-05-30 19:29:50 -0400

    Initial commit

---
+++ content.js
@@ -0,0 +1,34 @@
+function main() {
+    let styleFound = false;
+    let all = document.getElementsByTagName('*');
+
+    for (let i = 0; i < all.length; ++i) {
+        if (all[i].hasAttribute('style')) {
+            styleFound = true;
+            console.log('Inline style found');
+            break;
+        } else if (all[i].tagName === 'STYLE') {
+            styleFound = true;
+            console.log('Style tag found');
+            break;
+        } else if (all[i].tagName === 'LINK') {
+            if (all[i].rel === 'stylesheet') {
+                styleFound = true;
+                console.log('Stylesheet found');
+                break;
+            }
+        }
+    }
+
+    if (styleFound === false) {
+        console.log('No styling found');
+        console.log('Inserting new styling');
+
+        let body = document.getElementsByTagName('body')[0];
+        body.style.maxWidth = '960px';
+        body.style.marginLeft = 'auto';
+        body.style.marginRight = 'auto';
+    }
+}
+
+$(document).ready(main);
