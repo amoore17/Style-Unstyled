5914596fd38dbd26bd9ce1cb279e8a880303a986
Austin Moore <am058613@ohio.edu>
2019-05-30 22:03:56 -0400

    Changed indentation to 2 spaces

--- content.js
+++ content.js
@@ -1,37 +1,37 @@
 function main() {
-    let styleFound = false;
-    let all = document.getElementsByTagName('*');
+  let styleFound = false;
+  let all = document.getElementsByTagName('*');
 
-    for (let i = 0; i < all.length; ++i) {
-        if (all[i].hasAttribute('style')) {
-            styleFound = true;
-            console.log('Inline style found');
-            break;
-        } else if (all[i].tagName === 'STYLE') {
-            styleFound = true;
-            console.log('Style tag found');
-            break;
-        } else if (all[i].tagName === 'LINK') {
-            if (all[i].rel === 'stylesheet') {
-                styleFound = true;
-                console.log('Stylesheet found');
-                break;
-            }
-        }
+  for (let i = 0; i < all.length; ++i) {
+    if (all[i].hasAttribute('style')) {
+      styleFound = true;
+      console.log('Inline style found');
+      break;
+    } else if (all[i].tagName === 'STYLE') {
+      styleFound = true;
+      console.log('Style tag found');
+      break;
+    } else if (all[i].tagName === 'LINK') {
+      if (all[i].rel === 'stylesheet') {
+        styleFound = true;
+        console.log('Stylesheet found');
+        break;
+      }
     }
+  }
 
-    if (styleFound === false) {
-        console.log('No styling found');
-        console.log('Inserting new styling');
+  if (styleFound === false) {
+    console.log('No styling found');
+    console.log('Inserting new styling');
 
-        let body = document.getElementsByTagName('body')[0];
-        body.style.maxWidth = '960px';
-        body.style.marginLeft = 'auto';
-        body.style.marginRight = 'auto';
-    }
+    let body = document.getElementsByTagName('body')[0];
+    body.style.maxWidth = '960px';
+    body.style.marginLeft = 'auto';
+    body.style.marginRight = 'auto';
+  }
 }
 
 if (document.readyState !== 'loading')
-    main();
+  main();
 else
-    document.addEventListener('DOMContentLoaded', main);
+  document.addEventListener('DOMContentLoaded', main);
