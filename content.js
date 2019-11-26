c117b6e025ebb5bc86bdac9f036cb8ba641fe406
Austin Moore <amoore17@protonmail.com>
2019-11-25 23:59:46 -0500

    Added script check and timeout for dynamic content

--- content.js
+++ content.js
@@ -4,6 +4,7 @@
  * Function:    main
  * Description: If there is no CSS on the page, apply a default styling
  * Parameters:  void
+ * Returns:     void
  */
 function main() {
   let styleFound = false;
@@ -38,7 +39,38 @@ function main() {
   }
 }
 
-if (document.readyState !== 'loading')
-  main();
-else
-  document.addEventListener('DOMContentLoaded', main);
+let scriptCheckCount = 0;
+const maxScriptChecks = 10;
+
+/*
+ * Function:         scriptCheck
+ * Description:      Checks if the last script on the page has finished loading. If it has not, this function polls
+ *                   before timing out and calling main()
+ * Parameters:       void
+ * Global Variables: scriptCheckCount (int), maxScriptChecks (int)
+ * Notes:            scriptCheckCount - The amount of times the script has been checked for loading
+ *                   maxScriptChecks  - The maximum amount of times to check if the last script has been loaded
+ * Returns:     void
+ */
+function scriptCheck() {
+  let scripts = document.querySelectorAll('script');
+
+  if (scripts.length > 0) {
+    if (scripts[scripts.length - 1].readyState !== 'complete' && scriptCheckCount < maxScriptChecks) {
+      ++scriptCheckCount;
+      setTimeout(scriptCheck, 1000);
+    } else if (scriptCheckCount >= maxScriptChecks) {
+      console.log('One script failed to complete within timeout period. Testing for CSS now.');
+      console.log(scripts[scripts.length - 1]);
+      main();
+    }
+  } else {
+    main();
+  }
+}
+
+if (document.readyState !== 'loading') {
+  scriptCheck();
+} else {
+  document.addEventListener('DOMContentLoaded', scriptCheck);
+}
